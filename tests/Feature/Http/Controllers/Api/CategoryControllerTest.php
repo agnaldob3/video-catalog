a5cc57283ea65a1551f960dfaa3cc7c082f0ee57
agnaldo <agnaldootb@gmail.com>
2021-09-23 14:33:09 -0300

    Feature: Implementacao de resource em categoria para os testes.

--- tests/Feature/Http/Controllers/Api/CategoryControllerTest.php
+++ tests/Feature/Http/Controllers/Api/CategoryControllerTest.php
@@ -4,79 +4,143 @@ namespace Tests\Feature\Http\Controllers\Api;
 
 use App\Models\Category;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Http\Resources\CategoryResource;
+use Tests\Traits\TestValidations;
+use Tests\Traits\TestSaves;
+use Tests\Traits\TestResources;
 
 class CategoryControllerTest extends TestCase
 {
-    use DatabaseMigrations;
+    use DatabaseMigrations, TestValidations, TestSaves, TestResources;
+
+    private $serializedFields = [
+        'id',
+        'name',
+        'description',
+        'is_active',
+        'created_at',
+        'updated_at',
+        'deleted_at'
+    ];
+
+    private $category;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $this->category = factory(Category::class)->create();
+    }
 
     public function testIndex()
     {
-        $category = factory(Category::class)->create();
         $response = $this->get(route('categories.index'));
-        $response->assertStatus(200)
-        ->assertJson([$category->toArray()]);
+
+        $response
+            ->assertStatus(200)
+            ->assertJson([
+                'meta' => ['per_page' => 15]
+            ])
+            ->assertJsonStructure([
+                'data' => [
+                    '*' => $this->serializedFields
+                ],
+                'links' => [],
+                'meta' => [],
+            ]);
+
+        $resource = CategoryResource::collection(collect([$this->category]));
+        $this->assertResource($response, $resource);
+
     }
 
     public function testShow()
     {
-        $category = factory(Category::class)->create();
-        $response = $this->get(route('categories.show',['category' => $category->id]));
-        $response->assertStatus(200)
-            ->assertJson($category->toArray());
-    }
+        $response = $this->get(route('categories.show', ['category' => $this->category->id]));
 
-    public function testStore()
-    {
-        $response = $this->json('POST',route('categories.store',[
-            'name' => 'teste'
-        ]));
-
-        $id = $response->json('id');
-        $category = Category::find($id);
-
-        $response->assertStatus(201)
-            ->assertJson($category->toarray());
-        $this->assertTrue($response->json('is_active'));
-        $this->assertNull($response->json('description'));
-    }
-
-    public function testUpdate()
-    {
-        $category = factory(Category::class)->create([
-            'is_active' => false
-        ]);
-
-        $response = $this->json('PUT',route('categories.update',
-            [ 'category' => $category->id]),
-            [
-                'name'=> 'teste 2',
-                'description'=> 'teste',
-                'is_active' => true
+        $response
+            ->assertStatus(200)
+            ->assertJsonStructure([
+                'data' => $this->serializedFields
             ]);
 
-        $id = $response->json('id');
-        $category = Category::find($id);
-
-        $response->assertStatus(200)
-            ->assertJson($category->toarray())
-            ->assertJsonFragment([
-                'description'=> 'teste',
-                'is_active' => true
-            ]);
+        $id = $response->json('data.id');
+        $resource = new CategoryResource(Category::find($id));
+        $this->assertResource($response, $resource);
     }
 
-    public function testDelete(){
-        $category = factory(Category::class)->create();
 
-        $this->json('DELETE',route('categories.destroy',
-            [ 'category' => $category->id]));
+//    public function testShow()
+//    {
+//        $category = factory(Category::class)->create();
+//        $response = $this->get(route('categories.show',['category' => $category->id]));
+//        $response->assertStatus(200)
+//            ->assertJson($category->toArray());
+//    }
+//
+//    public function testStore()
+//    {
+//        $response = $this->json('POST',route('categories.store',[
+//            'name' => 'teste'
+//        ]));
+//
+//        $id = $response->json('id');
+//        $category = Category::find($id);
+//
+//        $response->assertStatus(201)
+//            ->assertJson($category->toarray());
+//        $this->assertTrue($response->json('is_active'));
+//        $this->assertNull($response->json('description'));
+//    }
+//
+//    public function testUpdate()
+//    {
+//        $category = factory(Category::class)->create([
+//            'is_active' => false
+//        ]);
+//
+//        $response = $this->json('PUT',route('categories.update',
+//            [ 'category' => $category->id]),
+//            [
+//                'name'=> 'teste 2',
+//                'description'=> 'teste',
+//                'is_active' => true
+//            ]);
+//
+//        $id = $response->json('id');
+//        $category = Category::find($id);
+//
+//        $response->assertStatus(200)
+//            ->assertJson($category->toarray())
+//            ->assertJsonFragment([
+//                'description'=> 'teste',
+//                'is_active' => true
+//            ]);
+//    }
+//
+//    public function testDelete(){
+//        $category = factory(Category::class)->create();
+//
+//        $this->json('DELETE',route('categories.destroy',
+//            [ 'category' => $category->id]));
+//
+//        $category = Category::find($category->id);
+//
+//        $this->assertNull($category);
+//
+//    }
+    protected function model()
+    {
+        return Category::class;
+    }
 
-        $category = Category::find($category->id);
+    protected function routeStore()
+    {
+        return route('categories.store');
+    }
 
-        $this->assertNull($category);
-
+    protected function routeUpdate()
+    {
+        return route('categories.update', ['category' => $this->category->id]);
     }
 }
